6c7664f818ce2ba097f8148c1c0c5ba60a183991
Alex Garai <alex.garai@somorrostro.com>
2024-12-17 11:43:41 +0100

    Añadidos constructores

--- src/clasesDatos/Empleado.java
+++ src/clasesDatos/Empleado.java
@@ -8,4 +8,54 @@ public class Empleado {
 	private int edad;
 	private boolean clubSocial; //indica si el empleado está en el club social de la empresa
 	private float sueldo;
+	
+	/////////////////// CONSTRUCTORES //////////////////
+	/**
+	 * Constructor completo
+	 * @param dni
+	 * @param nombre
+	 * @param apellidos
+	 * @param categoria
+	 * @param edad
+	 * @param clubSocial indica si el empleado está en el club social de la empresa
+	 * @param sueldo
+	 */
+	public Empleado(String dni, String nombre, String apellidos, String categoria, int edad, boolean clubSocial,
+			float sueldo) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.categoria = categoria;
+		this.edad = edad;
+		this.clubSocial = clubSocial;
+		this.sueldo = sueldo;
+	}
+	
+
+	// Constructor parcial
+	/**
+	 * @param dni
+	 * @param nombre
+	 * @param apellidos
+	 */
+	public Empleado(String dni, String nombre, String apellidos) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.categoria = "Base";
+		this.edad = 0;
+		this.clubSocial = false;
+		this.sueldo = (float) 0.0;
+		
+	}
+	
+
+
+	
+	
+	
 }
+
+
+
+
